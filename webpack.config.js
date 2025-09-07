c02112dbd14e03af173883f8f4861a0f9e1d511b
Lucas Toledo <lucas_nahueltoledo@hotmail.com>
2025-09-07 17:28:38 +0200

    correccion typo

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/utils/index.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
