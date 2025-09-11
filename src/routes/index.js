611e899b4786f914c293f8c59037e414c8e5981e
Lucas Toledo <lucas_nahueltoledo@hotmail.com>
2025-09-11 17:52:27 +0200

    logica de renderizado de contenido segun id de hash

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from "../templates/Header.js";
 import Home from "../pages/Home.js";
 import Character from "../pages/Character.js";
 import Error404 from "../pages/Error404.js";
+import getHash from "../utils/getHash.js";
+import resolveRoutes from "../utils/resolveRoutes.js";
 
 const routes = {
   "/": Home,
@@ -12,12 +14,12 @@ const routes = {
 const router = async () => {
   const header = document.getElementById("header");
   const content = document.getElementById("content");
-  if (header) {
-    header.innerHTML = await Header();
-  }
-  if (content) {
-    content.innerHTML = await Home(); // Renderiza la página principal
-  }
+
+  header.innerHTML = await Header();
+  let hash = getHash();
+  let route = await resolveRoutes(hash);
+  let render = routes[route] ? routes[route] : Error404;
+  content.innerHTML = await render();
 };
 
 export default router;
